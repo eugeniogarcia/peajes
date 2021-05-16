9159f15c361bf3a9d19ab21114e8a24f4090926f
Eugenio Garcia San Martin <egsmartin@gmail.com>
2021-05-16 21:41:36 +0200

    Configuracion

--- go/config.go
+++ go/config.go
@@ -1,26 +1,25 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+
 	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-    Server struct {
-        Port string `yaml:"port"`
-        Host string `yaml:"host"`
-    } `yaml:"isu"`
+	Server struct {
+		Port string `yaml:"port"`
+		Host string `yaml:"host"`
+	} `yaml:"isu"`
+
 	Frecuencia int `yaml:"frecuencia"`
 
-    BatchIds []struct {
-        batchid string `yaml:"batch"`
-    } `yaml:"batchs"`
-
-	ListaBatchs string `yaml:lista`
+	ListaBatchs string `yaml:"lista"`
 }
 
-func cargar(conf *Config){
+func cargar(conf *Config) {
 	f, err := os.Open("config.yml")
 	if err != nil {
 		processError(err)
@@ -30,11 +29,23 @@ func cargar(conf *Config){
 	decoder := yaml.NewDecoder(f)
 	err = decoder.Decode(conf)
 	if err != nil {
-		processError(err)
+		processError(fmt.Errorf("no se ha especificado la lista de batchs: %s", err))
+	}
+	if conf.ListaBatchs == "" {
+		processError(errors.New("no se ha especificado la lista de batchs"))
+	}
+	if conf.Server.Host == "" {
+		processError(errors.New("no se ha especificado el host de ISU"))
+	}
+	if conf.Server.Port == "" {
+		processError(errors.New("no se ha especificado el puerto de ISU"))
+	}
+	if conf.Frecuencia == 0 {
+		conf.Frecuencia = 60
 	}
 }
 
 func processError(err error) {
-    fmt.Println(err)
-    os.Exit(2)
+	fmt.Println(err)
+	os.Exit(2)
 }
