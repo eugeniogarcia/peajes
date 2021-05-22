d9f295bb7e0d923be5428a96e847ba7e4dd5deb7
Eugenio Garcia San Martin <egsmartin@gmail.com>
2021-05-22 23:53:33 +0200

    tunea

--- node/isu/migracionISU.js
+++ node/isu/migracionISU.js
@@ -36,9 +36,9 @@ for (var i=0;i<num_batches;i++){
 }
 
 //Probabilidad de que falle en %
-probabilidad_fallo=5;
+probabilidad_fallo=10;
 //Probabilidad de que cancele en por mil
-probabilidad_cancelar = 1;
+probabilidad_cancelar = 20;
 //Duracion media en segundos
 probabilidad_duracion_media = 30;
 
